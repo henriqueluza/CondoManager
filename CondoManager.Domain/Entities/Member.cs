99456b42c66087c3e5c4306762b9dd7fee3c2e90
Henrique Luza <henriqueluza@gmail.com>
2026-02-25 17:22:27 -0300

    Corrige o Member para inglês

--- CondoManager.Domain/Entities/Member.cs
+++ CondoManager.Domain/Entities/Member.cs
@@ -5,9 +5,9 @@ namespace CondoManager.Domain.Entities;
 public abstract class Member
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string NomeCompleto { get; set; }
+    public string Name { get; set; }
     public string Email {get; set;}
     public string Cpf {get; set;}
-    public DateOnly DataNascimento {get; set;}
+    public DateOnly DateOfBirth {get; set;}
     public MemberRole Role {get; set;}
 }
