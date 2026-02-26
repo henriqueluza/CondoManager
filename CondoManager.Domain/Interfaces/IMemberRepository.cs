51bef04de9a9a62dd9403e3a336a09f2f54e6dc3
Henrique Luza <henriqueluza@gmail.com>
2026-02-26 08:48:55 -0300

    Cria e preenche Interface IMemberRepository

---
+++ CondoManager.Domain/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+using CondoManager.Domain.Entities;
+
+namespace CondoManager.Domain.Interfaces;
+
+public interface IMemberRepository
+{
+    Task<Member?> GetMember(Guid id);
+    Task<ICollection<Member>> GetAll(Guid condominiumId);
+    Task Add(Member member);
+    Task Update(Member member);
+    Task Delete(Guid id);
+}
+
