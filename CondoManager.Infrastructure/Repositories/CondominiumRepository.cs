23ac964874329b82d26a1f1326a2c8067cc9a601
Henrique Luza <henriqueluza@gmail.com>
2026-02-26 14:10:50 -0300

    Adiciona imports

--- CondoManager.Infrastructure/Repositories/CondominiumRepository.cs
+++ CondoManager.Infrastructure/Repositories/CondominiumRepository.cs
@@ -1,3 +1,8 @@
+using CondoManager.Domain.Interfaces;
+using CondoManager.Infrastructure.Data;
+using CondoManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace CondoManager.Infrastructure.Repositories;
 
 public class CondominiumRepository
