3eccae184636b886abc14c08f1b1e8cbafc2487b
Henrique Luza <henriqueluza@gmail.com>
2026-02-28 21:20:34 -0300

    Adiciona campos Cpf e Telefone no User

--- CondoManager.Domain/Entities/User.cs
+++ CondoManager.Domain/Entities/User.cs
@@ -5,6 +5,8 @@ public class User
     public Guid Id { get; set; } =  Guid.NewGuid();
     public string Name { get; set; }
     public string Email { get; set; }
+    public string Cpf { get; set; }
+    public string Cellphone { get; set; }
     public string PasswordHash { get; set; }
     public ICollection<Condominium> Condominiums { get; set; } = new List<Condominium>();
 }
