580dbe1a6dc948b0f1b18ca48bc3d5dca7edd470
Henrique Luza <henriqueluza@gmail.com>
2026-03-05 12:05:50 -0300

    Adiciona método register dentro do AuthServices

--- frontend/condo-manager-web/src/app/core/services/auth-services.ts
+++ frontend/condo-manager-web/src/app/core/services/auth-services.ts
@@ -6,13 +6,18 @@ import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {
+export class AuthServices {
   private http = inject(HttpClient);
 
   login(email:string, password:string) {
 
     const body = { email, password };
     return this.http.post(`${environment.apiUrl}/api/user/login`, body)
+  }
+
+  register(email:string, password:string, cpf:string, cellphone:string, name:string) {
+    const body = {name, email, cpf, cellphone, password};
+    return this.http.post(`${environment.apiUrl}/api/user/register`, body)
   }
 }
 
