eb3705791982c43b913e73b05c0d00796c26be94
Henrique Luza <henriqueluza@gmail.com>
2026-03-05 14:17:20 -0300

    Apaga tudo relacionado ao angular para trocar pelo react

--- frontend/condo-manager-web/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {Auth} from './features/auth/auth';
-import {Dashboard} from "./features/dashboard/dashboard";
-
-export const routes: Routes = [
-  {path: "auth", component: Auth},
-  { path: 'dashboard', component: Dashboard },
-  {path: "", redirectTo: "auth", pathMatch: "full"}
-];
