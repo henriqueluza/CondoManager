385a36ba7d0e38fef98346399fdec1c60120dac2
Henrique Luza <henriqueluza@gmail.com>
2026-03-09 16:06:09 -0300

    Refatora código e implementa authService

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
-function App() {
-    return (
-        <div className="text-red-500 text-2xl">
-            <h1>CondoManager</h1>
-        </div>
-    )
-}
+import AppRouter from './router/AppRouter'
 
-export default App
+export default function App() {
+    return <AppRouter/>
+}
---
+++ frontend/src/features/auth/authService.ts
@@ -0,0 +1,9 @@
+import api from '../../services/api.ts'
+
+export async function login(email: string, password: string) {
+    await api.post('user/login', {email, password})
+}
+
+export async function register(name: string, email: string, password: string, cellphone: string, cpf: string) {
+    await api.post('user/register', {name, email, password, cpf, cellphone})
+}
