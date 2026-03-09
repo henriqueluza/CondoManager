5c50b9be4bb2286e118aec70354eebdd0ea8e92c
Henrique Luza <henriqueluza@gmail.com>
2026-03-09 17:15:04 -0300

    Adiciona getMe no authService

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -7,3 +7,8 @@ export async function login(email: string, password: string) {
 export async function register(name: string, email: string, password: string, cellphone: string, cpf: string) {
     await api.post('user/register', {name, email, password, cpf, cellphone})
 }
+
+export async function getMe() {
+    const response = await api.get('/user/me')
+    return response.data
+}
